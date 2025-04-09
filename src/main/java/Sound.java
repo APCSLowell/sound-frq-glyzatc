1039f42446a8f0e278bffd4ae610eddf3b4bd6e8
glyzatc <gltolentino@s.sfusd.edu>
2025-04-09 10:54:16 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -15,8 +15,16 @@ public class Sound
    */
   public int limitAmplitude(int limit)
   {  
-    /* to be implemented in part (a) */
-    return 0;
+    int count = 0;
+    for (int i = 0; i < sample.length; i++){
+      if (samples [i] > limit;
+      count++;
+    } else if (samples [i] = - limit){
+      samples [i] = - limit;
+      count++;
+    }
+  }
+    return count;
   }
 
 
@@ -28,6 +36,14 @@ public class Sound
    */
   public void trimSilenceFromBeginning()
   {
-    /* to be implemented in part (b) */
+    int s = 0;
+    while (samples [s] == 0)
+      s++;
+    int [] newSamples = new int [samples.length - s];
+    for (int i = 0; i < newSamples.length; i++)
+      newSamples [i] = samples [i + s];
+    samples = newSamples;
+  }
+}
   }
 }
